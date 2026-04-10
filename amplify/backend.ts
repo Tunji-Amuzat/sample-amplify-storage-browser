ecd7f526b85a499f34f13dbbee79aad65eb15436
Tunji-Amuzat <164157106+Tunji-Amuzat@users.noreply.github.com>
2026-04-10 07:35:19 +0000

    update storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,20 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { storage, secondaryStorage } from './storage/resource';
+import { storage } from './storage/resource';
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
-  storage, 
-  secondaryStorage
+  storage
+});
+
+// Reference an existing external S3 bucket
+backend.addOutput({
+  storage: {
+    aws_region: 'us-east-1',
+    bucket_name: 'alarrt-test-bucket'
+  }
 });
