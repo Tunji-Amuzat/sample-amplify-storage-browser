264de806e67f1a59172ff1b87fdf2c26d4e171bb
Slycreator <chinonso1amaechi@gmail.com>
2026-04-14 10:46:19 +0100

    add bulk download action using zipDownloadHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import {
   createAmplifyAuthAdapter,
   createStorageBrowser,
-  zipDownloadHandler,
 } from '@aws-amplify/ui-react-storage/browser';
+import type { ActionHandler } from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
 import './App.css';
 
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
+import { getUrl } from 'aws-amplify/storage';
 import { Authenticator, Button } from '@aws-amplify/ui-react';
 Amplify.configure(config);
+
+const bulkDownloadHandler: ActionHandler = ({ data }) => {
+  const { key } = data as { key: string };
+  const result = getUrl({ path: key }).then(({ url }) => {
+    const a = document.createElement('a');
+    a.href = url.toString();
+    a.download = key.split('/').pop() ?? key;
+    a.click();
+    return { status: 'COMPLETE' as const };
+  });
+  return { result };
+};
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
   actions: {
     custom: {
       bulkDownload: {
-        handler: zipDownloadHandler,
+        handler: bulkDownloadHandler,
         actionListItem: {
           icon: 'download',
           label: 'Download',
