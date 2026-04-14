b198c84205a7764bda2feda364e07925717b0c70
Slycreator <chinonso1amaechi@gmail.com>
2026-04-14 12:47:01 +0100

    remove logo and branding from header

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import './App.css';
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button } from '@aws-amplify/ui-react';
-import gtcoLogo from './gtco-logo.png';
 Amplify.configure(config);
 
 const { StorageBrowser } = createStorageBrowser({
@@ -18,16 +17,9 @@ const { StorageBrowser } = createStorageBrowser({
 function App() {
   return (
     <Authenticator hideSignUp={true}>
-      {({ signOut, user }) => (
+      {({ signOut }) => (
         <>
           <div className="header">
-            <div className="header-left">
-              <img src={gtcoLogo} alt="GTCO" className="header-logo" />
-              <div className="header-text">
-                <span className="header-brand">Guaranty Trust Bank</span>
-                <span className="header-greeting">Hello, {user?.username}</span>
-              </div>
-            </div>
             <Button onClick={signOut}>Sign out</Button>
           </div>
           <StorageBrowser />
