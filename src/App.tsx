388710bdba911a9cde620e1410cfeadc84bc7ecb
Slycreator <chinonso1amaechi@gmail.com>
2026-04-14 12:44:15 +0100

    use GTCO logo and GTBank orange theme in header

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,12 @@ import './App.css';
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button } from '@aws-amplify/ui-react';
+import gtcoLogo from './gtco-logo.png';
 Amplify.configure(config);
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
-
-const GTCOLogo = () => (
-  <svg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg">
-    <rect width="60" height="60" fill="#e8450a" />
-    <rect x="32" y="10" width="14" height="14" fill="white" />
-    <text x="30" y="42" fontFamily="Arial, sans-serif" fontWeight="bold" fontSize="16" fill="white" textAnchor="middle">GTCO</text>
-  </svg>
-);
 
 function App() {
   return (
@@ -29,7 +22,7 @@ function App() {
         <>
           <div className="header">
             <div className="header-left">
-              <GTCOLogo />
+              <img src={gtcoLogo} alt="GTCO" className="header-logo" />
               <div className="header-text">
                 <span className="header-brand">Guaranty Trust Bank</span>
                 <span className="header-greeting">Hello, {user?.username}</span>
